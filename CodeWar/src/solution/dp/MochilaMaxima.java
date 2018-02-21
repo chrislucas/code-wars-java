8e5d240ee6c2774bfb1a89d39c0870b2e26cd9fe
chrislucas <christoffer.luccas@gmail.com>
2018-02-21 17:06:13 -0300

    estudo sobre dynamic programming, implementando algoritmos utilizando a abordagem bottom up

--- CodeWar/src/solution/dp/MochilaMaxima.java
+++ CodeWar/src/solution/dp/MochilaMaxima.java
@@ -61,20 +61,55 @@ public class MochilaMaxima {
     /**
      *
      * */
-    public static int solverBottomUp(int capacity, int weights[], int values [], boolean [] objects) {
-        int l = objects.length, c = capacity + 1;
-        int memo [][] = new int[l][c];
-        for (int i = 0; i < l ; i++) {
-            for (int j = 0; j < c; j++) {
-                
+    private static int solverBottomUp(int capacity, int weights[], int values [], boolean [] objects) {
+        int l = objects.length;
+        int memo [][] = new int[l+1][capacity+1];
+        for (int object = 0; object <= l ; object++) {
+            for (int weight = 0; weight <= capacity; weight++) {
+                if(object==0 || weight==0)
+                    memo[object][weight] = 0;
+                else if(weights[object-1] <= weight) {
+                    int o = values[object-1] + memo[object-1][weight-weights[object-1]];
+                    memo[object][weight] = o > memo[object-1][weight] ? o : memo[object-1][weight];
+                }
+                else {
+                    memo[object][weight] = memo[object-1][weight];
+                }
             }
         }
-
-        return memo[l-1][c];
+        return memo[l][capacity];
     }
 
+    private static int solverBottomUpV2(int capacity, int weights[], int values [], boolean [] objects) {
+        int l = objects.length;
+        /**
+         * Matriz bidimensional de estados.
+         *
+         * As listas representam a quantidade de objetos usados para resolver os subproblemas
+         * do problema da mochila 0-1
+         *
+         * o loop abaixo comeca com 1 o que quer dizer que comecamos usando 1 objeto do vetor
+         * values[] e aumentamos a qauntidade ate os N objetos dispostos nesse problema
+         * */
+        int memo [][] = new int[l+1][capacity+1];
+        for(int i=0; i<capacity+1; i++)
+            memo[0][i] = 0;
+        for (int qObjects = 1; qObjects<=l ; qObjects++) {
+            for (int weight=1; weight <=capacity; weight++) {
+                if(weights[qObjects-1] <= weight) {
+                    int cow = weight-weights[qObjects-1];
+                    int o = values[qObjects-1] + memo[qObjects-1][cow];
+                    memo[qObjects][weight] = o > memo[qObjects-1][weight] ? o : memo[qObjects-1][weight];
+                }
+                else {
+                    memo[qObjects][weight] = memo[qObjects-1][weight];
+                }
+            }
+        }
+        return memo[l][capacity];
+    }
 
-    public static void test1() {
+    private static void testTopDown1() {
         int [] cap = {3, 50};
         int [][] weights = {
             {3,1,2}
@@ -92,11 +127,30 @@ public class MochilaMaxima {
             if(objects[i])
                 System.out.printf("%d ", i);
         }
+        System.out.println("");
+    }
 
-        System.out.println();
+    private static void testBottomUp1() {
+        int [] cap = {3, 50};
+        int [][] weights = {
+             {3,1,2}
+            ,{10,20,30}
+        }
+        , values = {
+            {5,3,3}
+            ,{60,100,120}
+        };
+        int idx = 0;
+        boolean [] objects = new boolean[weights[idx].length];
+        System.out.println(solverBottomUpV2(cap[idx], weights[idx], values[idx], objects));
+        for(int i=0; i<objects.length; i++) {
+            if(objects[i])
+                System.out.printf("%d ", i);
+        }
+        System.out.println("");
     }
 
     public static void main(String[] args) {
-        test1();
+        testBottomUp1();
     }
 }
