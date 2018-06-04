d807bd94796054dbb32e91ddd90434bb50990b11
chrislucas <christoffer.luccas@gmail.com>
2018-06-04 18:22:35 -0300

    merging

---
+++ solutions/math/numbers/ReversePolishNotation/src/solution/Solution.java
@@ -0,0 +1,18 @@
+package solution;
+
+public class Solution {
+
+    /**
+     *
+     * https://www.codewars.com/kata/52f78966747862fc9a0009ae/train/java
+     * */
+
+    public double evaluate(String expr) {
+        // TODO: Your awesome code here
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
