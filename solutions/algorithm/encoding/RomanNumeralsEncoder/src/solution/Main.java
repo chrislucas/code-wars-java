d4d11efa82f42d39c8f7d61deb18ff804b0f8023
chrislucas <christoffer.luccas@gmail.com>
2018-08-14 19:05:16 -0300

    estudos

--- solutions/algorithm/encoding/RomanNumeralsEncoder/src/solution/Main.java
+++ solutions/algorithm/encoding/RomanNumeralsEncoder/src/solution/Main.java
@@ -21,28 +21,75 @@ public class Main {
         romanMap.put(1000, "M");
     }
 
+    private String getChar(int n) {
+        StringBuilder s = new StringBuilder();
+        if (n > 1 && n < 5) {
+            if (n == 4) {
+                s.append("IV");
+            }
+            else {
+                for (int i = 0; i < n ; i++) {
+                    s.append("I");
+                }
+            }
+        }
+
+        else if (n > 5 && n < 10) {
+            if (n == 9) {
+                s.append("IX");
+            }
+            else {
+                s.append("V");
+                for (int i = 0; i < n ; i++) {
+                    s.append("I");
+                }
+            }
+        }
+
+        else if (n > 10 && n < 100) {
+            int d = n % 10;
+            int u = n / 10;
+        }
+
+        else if (n > 100 && n < 1000) {
+
+        }
+
+        else {
+
+        }
+
+        return s.toString();
+    }
+
     private String process(int m, int digits) {
         if (romanMap.containsKey(m))
             return romanMap.get(m);
         else {
             StringBuilder sb = new StringBuilder();
+            int mutiple = (int)Math.pow(10, digits);
+            while (m > 0) {
+                int n = m - (m % mutiple);
+                sb.append(getChar(n));
+                mutiple /= 10;
+            }
             return sb.toString();
         }
     }
 
-    private int getDigits(int n) {
+    private int getQDigits(int n) {
         return (int) Math.log10(n);
     }
 
     public String solution(int n) {
-        int e = getDigits(n);
-        int digits = (int)Math.pow(10, e);
+        int e = getQDigits(n);
+        int mutiple = (int)Math.pow(10, e);
         StringBuilder sb = new StringBuilder();
         while (n>9) {
-            int m = n - (n % digits);
-            sb.append(process(m, getDigits(m)));
-            digits /= 10;
-            n -= m;
+            int o = n - (n % mutiple);
+            sb.append(process(o, getQDigits(o)));
+            mutiple /= 10;
+            n -= o;
         }
         return sb.append(process(n, 1)).toString();
     }
