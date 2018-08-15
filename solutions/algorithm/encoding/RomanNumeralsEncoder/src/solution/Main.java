a5d36fa72fb5959a904da513b4cf22d0d8182192
chrislucas <christoffer.luccas@gmail.com>
2018-08-15 12:01:23 -0300

    estudo

--- solutions/algorithm/encoding/RomanNumeralsEncoder/src/solution/Main.java
+++ solutions/algorithm/encoding/RomanNumeralsEncoder/src/solution/Main.java
@@ -3,11 +3,12 @@ package solution;
 
 import java.util.LinkedHashMap;
 
-public class Main {
+/**
+ * http://www.codewars.com/kata/roman-numerals-encoder/train/java
+ * DONE
+ **/
 
-    /**
-     * http://www.codewars.com/kata/roman-numerals-encoder/train/java
-     **/
+public class Main {
 
     static final LinkedHashMap<Integer, String> romanMap = new LinkedHashMap<>();
 
@@ -21,44 +22,88 @@ public class Main {
         romanMap.put(1000, "M");
     }
 
+    private int getQDigits(int n) {
+        return (int) Math.log10(n);
+    }
+
     private String getChar(int n) {
         StringBuilder s = new StringBuilder();
-        if (n > 1 && n < 5) {
-            if (n == 4) {
+        if (n > 1 && n < 10) {
+            if (n < 4) {
+                for (int i = 0; i < n ; i++) {
+                    s.append("I");
+                }
+            }
+            else if (n == 4) {
                 s.append("IV");
             }
-            else {
-                for (int i = 0; i < n ; i++) {
+            else if(n > 5 && n < 9) {
+                s.append("V");
+                for (int i = 0; i < n-5 ; i++) {
                     s.append("I");
                 }
             }
-        }
-
-        else if (n > 5 && n < 10) {
-            if (n == 9) {
+            else {
                 s.append("IX");
             }
-            else {
-                s.append("V");
-                for (int i = 0; i < n ; i++) {
-                    s.append("I");
+        }
+        else if (n > 9 && n < 100) {
+            int u = (n % 10);
+            int c = n - u;
+            int d = n/10;
+            if (c < 40) {
+                for (int i = 0; i<d ; i++) {
+                    s.append("X");
                 }
             }
+            else if (c == 40) {
+                s.append("XL");
+            }
+            else if (c < 90) {
+                s.append("L");
+                for (int i = 0; i<d-5; i++) {
+                    s.append("X");
+                }
+            }
+
+            else {
+                s.append("XC");
+            }
+
+            if (u > 0)
+                s.append(getChar(u));
         }
+        else if (n > 99 && n < 1000) {
+            int d = n % 100;
+            int c = n - d;
+            if (c < 400) {
+                for (int i = 0; i < c/100 ; i++) {
+                    s.append("C");
+                }
+            }
+            else if (c == 400) {
+                s.append("CD");
+            }
 
-        else if (n > 10 && n < 100) {
-            int d = n % 10;
-            int u = n / 10;
+            else if (c < 900) {
+                s.append("D");
+                for (int i = 0; i < c/100-5; i++) {
+                    s.append("C");
+                }
+            }
+            else {
+                s.append("CM");
+            }
+
+            if (d > 0) {
+                s.append(getChar(d));
+            }
         }
-
-        else if (n > 100 && n < 1000) {
-
-        }
-
         else {
-
+            for (int i = 0; i < n/1000 ; i++) {
+                s.append("M");
+            }
         }
-
         return s.toString();
     }
 
@@ -67,18 +112,15 @@ public class Main {
             return romanMap.get(m);
         else {
             StringBuilder sb = new StringBuilder();
-            int mutiple = (int)Math.pow(10, digits);
-            while (m > 0) {
+            int mutiple = (int) Math.pow(10, digits);
+            while (m > 9) {
                 int n = m - (m % mutiple);
                 sb.append(getChar(n));
                 mutiple /= 10;
+                m -= n;
             }
-            return sb.toString();
+            return m == 0 ? sb.toString() : sb.append(getChar(m)).toString();
         }
-    }
-
-    private int getQDigits(int n) {
-        return (int) Math.log10(n);
     }
 
     public String solution(int n) {
@@ -91,14 +133,19 @@ public class Main {
             mutiple /= 10;
             n -= o;
         }
-        return sb.append(process(n, 1)).toString();
+        return n == 0 ? sb.toString() : sb.append(process(n, 1)).toString();
     }
 
     public static void main(String[] args) {
         Main main = new Main();
+        for (int i = 1; i < 4000 ; i++) {
+            System.out.printf("%d %s\n", i, main.solution(i));
+        }
+        /*
         main.solution(17234);
         main.solution(17000);
         main.solution(1700);
         main.solution(170);
+        */
     }
 }
---
+++ solutions/string/regex/CountIPAddress/src/solution/Solution.java
@@ -0,0 +1,15 @@
+package solution;
+
+/**
+ * https://www.codewars.com/kata/526989a41034285187000de4/train/java
+ * */
+
+public class Solution {
+
+    public static long ipsBetween(String start, String end) {
+        return -1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
